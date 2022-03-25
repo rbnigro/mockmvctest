2f3933d5e4b65f34a6d467cbdd534b092f2d4a95
Ronney Nigro <ronneynigro@gmail.com>
2022-03-25 12:19:37 -0300

    Validating Input

--- src/main/java/br/com/springboot/cursojdevtreinamento/service/UsuarioService.java
+++ src/main/java/br/com/springboot/cursojdevtreinamento/service/UsuarioService.java
@@ -2,11 +2,17 @@ package br.com.springboot.cursojdevtreinamento.service;
 
 import br.com.springboot.cursojdevtreinamento.model.Usuario;
 import br.com.springboot.cursojdevtreinamento.repository.UsuarioRepository;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -23,14 +29,21 @@ public class UsuarioService {
     }
 
     public Usuario salvarUsuario(Usuario usuario) {
-        Usuario usuarioLocal = usuarioRepository.save(usuario);
+        Usuario usuarioLocal = null;
+
+        try {
+            usuarioLocal = usuarioRepository.save(usuario);
+        } catch(Exception e) {
+            System.out.println(e.toString());
+        }
         return usuarioLocal;
     }
 
     public HttpStatus deletarUsuario(Long idUser) {
         Optional<Usuario> usuarioLocal = usuarioRepository.findById(idUser);
+
         if (!usuarioLocal.isPresent()) {
-            return HttpStatus.NOT_FOUND;
+            return HttpStatus.NO_CONTENT;
         }
         usuarioRepository.deleteById(idUser);
         return HttpStatus.OK;
@@ -38,8 +51,6 @@ public class UsuarioService {
 
     public Optional<Usuario> buscarIdUsuario(@NotNull Long idUser) {
         Optional<Usuario> usuarioLocal = usuarioRepository.findById(idUser);
-
-        System.out.println(idUser);
 
         if (!usuarioLocal.isPresent()) {
             return Optional.empty();
@@ -66,4 +77,18 @@ public class UsuarioService {
         return listaUsuarioLocal;
     }
 
+    public boolean validarJsonUsuario(String inputJosn) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(DeserializationFeature.FAIL_ON_READING_DUP_TREE_KEY);
+        JsonFactory factory = mapper.getFactory();
+        JsonParser parser = factory.createParser(inputJosn);
+        JsonNode jsonObj = mapper.readTree(parser);
+        System.out.println(jsonObj.toString());
+
+        return true;
+    }
+
+    public boolean validarInputJson(@NotNull Usuario usuario) {
+        return  (usuario.getNome() != null && usuario.getIdade() != 0);
+    }
 }
